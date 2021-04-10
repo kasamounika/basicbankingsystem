bfaa82a3ca7cf67fdcd63bd9723273af5c851505
kasamounika <70064158+kasamounika@users.noreply.github.com>
2021-04-10 21:46:30 +0530

    Add files via upload

---
+++ transferHistory.php
@@ -0,0 +1,26 @@
+<html>
+<body center>
+<?php
+
+$user=$_POST['name'];
+$name1=$_POST['name1'];
+$amount=$_POST['amt'];
+
+$con = mysqli_connect("localhost","root","","transfer") ;
+$sql="insert into history(sender,receiver,amount) values('{$user}','{$name1}','{$amount}')";
+$result=mysqli_query($con,$sql);
+
+
+
+
+if($result){
+        echo "<script> alert('Hurray! Transcation successful');
+                        window.location='userData.php';
+                </script>";
+                    
+    }
+ 
+
+?>
+</body>
+</html>
